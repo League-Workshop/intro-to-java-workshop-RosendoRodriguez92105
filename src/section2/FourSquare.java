99242d0ee17f7685a1d5c2b3c0dc2a9fdd6fce8b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-03 13:30:05 -0700

    workshop robot

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -5,28 +5,37 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
-	// 2. Create a new Robot
+	Robot bigboi = new Robot();// 2. Create a new Robot
 
-	void go() {
+			void go() {
 		// 4. Make the robot move as fast as possible
-
+bigboi.setSpeed(500);
 		// 5. Set the pen width to 5
-
+bigboi.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+for(int i=0;i<4;i++) {
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
+				drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
+}
 	}
 
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
+		bigboi.setSpeed(500);
+		bigboi.penDown();
+		bigboi.move(200);
+		bigboi.turn(90);
+		bigboi.move(200);
+		bigboi.turn(90);
+		bigboi.move(200);
+		bigboi.turn(90);
+		bigboi.move(200);
 		
 	}
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot bigboi = new Robot();
+	bigboi.setSpeed(100);
+	bigboi.penDown();
+	for(int i=0;i<4;i++) {
+	bigboi.move(200);
+	bigboi.turn(90);
 	
-		
-		
+	
+	
+
 	}
+}
 }
