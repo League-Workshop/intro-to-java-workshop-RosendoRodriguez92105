823988e44c3f308275d97242d10d40199e6da9ab
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-10 13:30:13 -0700

    Strings

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -15,7 +15,7 @@ bigboi.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
 for(int i=0;i<4;i++) {
 			// 7. Set the pen color to random
-	
+bigboi.setRandomPenColor();
 			// 1. Call the drawSquare() method
 				drawSquare();
 	
@@ -25,7 +25,7 @@ for(int i=0;i<4;i++) {
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		
 		/* 3. Fill in the code to draw a square inside the method below. */
 		bigboi.setSpeed(500);
 		bigboi.penDown();
--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -6,15 +6,16 @@ public class CandyCrush {
 
 	public static void main(String[] args) {
 		// 1. Using a pop-up, ask the user who they don't like
-
+             String big =  JOptionPane.showInputDialog(null, "who dont you like?");
+              
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
-		JOptionPane.showMessageDialog(null, "You totally have a crush on Java!");
+		JOptionPane.showMessageDialog(null, "You totally have a crush on " +big);
 		// 3. Ask the user for the name of their best friend
-
+        String name =JOptionPane.showInputDialog("What is your best friends name");
+        System.out.println(name);
 		// 4. Tell them their best friend is as sweet as candy
+        JOptionPane.showMessageDialog(null, name+ "is are as sweet as candy");
 
 	} 
 }
-
-
 
